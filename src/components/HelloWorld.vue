f884008e1d52d2ef6596604f0a24cf9adf092d59
ralphxlaurence <ralph.batang@gmail.com>
2026-08-17 22:39:59 +0800

    Initial commit for develop

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -9,11 +9,6 @@ const count = ref(0)
 
 <template>
   <section id="center">
-    <div class="hero">
-      <img :src="heroImg" class="base" width="170" height="179" alt="" />
-      <img :src="vueLogo" class="framework" alt="Vue logo" />
-      <img :src="viteLogo" class="vite" alt="Vite logo" />
-    </div>
     <div>
       <h1>Get started</h1>
       <p>Edit <code>src/App.vue</code> and save to test <code>HMR</code></p>
